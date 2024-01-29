e21f74cba04ff3dc14f1b91236db30bf8bbb0d9b
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2024-01-28 20:53:06 -0500

    added link to homepage

--- tsheringgurung/src/App.js
+++ tsheringgurung/src/App.js
@@ -1,16 +1,20 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Navbar from "./pages/Navbar";
 import Footer from "./pages/Footer";
 
-
 function App() {
   return (
-    <div>
-      <Navbar/>
-      <Home/>
-      <Footer/>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
