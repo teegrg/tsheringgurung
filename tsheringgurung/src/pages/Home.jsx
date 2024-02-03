551d64abb2dbcfbc530c771f36290176263667cc
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2024-02-02 21:58:47 -0500

    added mui arrow icon

--- tsheringgurung/src/pages/Home.jsx
+++ tsheringgurung/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 // import { Link } from "react-router-dom"
 import React, { useState, useEffect } from "react";
-import About from "../component/About"
+import About from "../component/About";
 
 import "../style/home.css";
 import project1 from "./pic/project1.png";
@@ -8,7 +8,7 @@ import project2 from "./pic/splitz.png";
 import project3 from "./pic/namastecard.png";
 import project4 from "./pic/curator.png";
 
-import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
+import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward";
 
 function Home() {
   const [role, setRole] = useState("Developer");
@@ -36,21 +36,29 @@ function Home() {
       <About />
       <div className="project">
         <div className="project__1">
-          <a href="https://keepssake.netlify.app/" target="_blank">
-            <img className="img" src={project1} alt="pic" />
-            <div className="project__text">
-              <div className="title">KEEPSAKE <ArrowOutwardIcon/></div>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-              ipsam nisi aperiam quod itaque consectetur culpa, rerum aliquam
-              vel excepturi molestiae quas suscipit dolore eum maiores unde
-              exercitationem totam cum.
-            </div>
-          </a>
+          <img className="img" src={project1} alt="pic" />
+          <div className="project__text">
+            <a href="https://keepssake.netlify.app/" target="_blank">
+              <div className="title">
+                <div className="title__text keepsake__text">KEEPSAKE</div>
+                <ArrowOutwardIcon className="arrow keepsake__arrow" />
+              </div>
+            </a>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            ipsam nisi aperiam quod itaque consectetur culpa, rerum aliquam vel
+            excepturi molestiae quas suscipit dolore eum maiores unde
+            exercitationem totam cum.
+          </div>
         </div>
         <div className="project__2">
           <img className="img" src={project2} alt="pic" />
           <div className="project__text">
-          <div className="title">SPLITZ <ArrowOutwardIcon/></div>
+            <a href="https://splitzer.netlify.app/" target="_blank">
+              <div className="title">
+                <div className="title__text">SPLITZ</div>
+                <ArrowOutwardIcon className="arrow" />
+              </div>
+            </a>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse vitae
             ex voluptatum culpa hic eaque in sed. Ad in quas placeat distinctio
             cumque nobis molestias delectus. Itaque voluptas cumque tenetur?
@@ -59,7 +67,12 @@ function Home() {
         <div className="project__3">
           <img className="img" src={project3} alt="pic" />
           <div className="project__text">
-          <div className="title">NAMASTE CARDS <ArrowOutwardIcon/></div>
+            <a href="https://namastecards.onrender.com/" target="_blank">
+              <div className="title">
+                <div className="title__text card__text">NAMASTE CARDS</div>
+                <ArrowOutwardIcon className="arrow card__arrow" />
+              </div>
+            </a>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse vitae
             ex voluptatum culpa hic eaque in sed. Ad in quas placeat distinctio
             cumque nobis molestias delectus. Itaque voluptas cumque tenetur?
@@ -68,7 +81,12 @@ function Home() {
         <div className="project__4">
           <img className="img" src={project4} alt="pic" />
           <div className="project__text">
-          <div className="title">THE CURATOR <ArrowOutwardIcon/></div>
+            <a href="https://teegrg.github.io/portfolioProject/index.html" target="_blank">
+              <div className="title">
+                <div className="title__text">THE CURATOR</div>
+                <ArrowOutwardIcon className="arrow" />
+              </div>
+            </a>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse vitae
             ex voluptatum culpa hic eaque in sed. Ad in quas placeat distinctio
             cumque nobis molestias delectus. Itaque voluptas cumque tenetur?
