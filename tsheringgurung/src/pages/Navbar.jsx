1a0a7f2412ed0499ac1dffb5696f4f0c87a92375
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2024-02-03 14:45:30 -0500

    added funtionality when scrolled up

--- tsheringgurung/src/pages/Navbar.jsx
+++ tsheringgurung/src/pages/Navbar.jsx
@@ -1,10 +1,29 @@
 import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
+import { useState, useEffect } from "react";
 import "../style/navbar.css";
 import logo from "./pic/logo.png";
 
 function Navbar() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="nav">
+    <div className={`nav ${scrolled ? "scrolled" : ""}`}>
       <div>
         <ScrollLink to="home" smooth={true} offset={-50}>
           <img className="logo" src={logo} alt="logo" />
@@ -14,12 +33,18 @@ function Navbar() {
         <ScrollLink to="home" smooth={true} offset={-50}>
           <div className="nav__home">Home</div>
         </ScrollLink>
-        {/* about id linked to scrolllink */}
-        <ScrollLink to="about" smooth={true} offset={-50}> 
+        <ScrollLink to="about" smooth={true} offset={-50}>
           <div className="nav__about">About</div>
         </ScrollLink>
-        <div className="nav__contact"><a href="https://docs.google.com/document/d/1sLeu8KKqFF4cGS-zL7xrqLFb4uryli55/edit?usp=sharing&ouid=110595949783449698615&rtpof=true&sd=true" target="_blank" rel="noopener noreferrer">Resume</a></div>
-        
+        <div className="nav__contact">
+          <a
+            href="https://docs.google.com/document/d/1sLeu8KKqFF4cGS-zL7xrqLFb4uryli55/edit?usp=sharing&ouid=110595949783449698615&rtpof=true&sd=true"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Resume
+          </a>
+        </div>
       </div>
     </div>
   );
