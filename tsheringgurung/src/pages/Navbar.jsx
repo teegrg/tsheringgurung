86db97250f9aaf087c78a5101f2e9eec53b86f52
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2024-02-02 22:01:00 -0500

    installed react scroll

--- tsheringgurung/src/pages/Navbar.jsx
+++ tsheringgurung/src/pages/Navbar.jsx
@@ -1,23 +1,28 @@
-import { Link } from "react-router-dom"
-
-
-import "../style/navbar.css"
-import logo from "./pic/logo.png"
+import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
+import "../style/navbar.css";
+import logo from "./pic/logo.png";
 
 function Navbar() {
-    return (
-        <div className="nav">
-            <div >
-                <Link><img className="logo" src={logo} alt="logo" /></Link>
-            </div>
-            <div className="nav__side">
-                <Link to="/"><div>Home</div></Link>
-                <Link to="/about" ><div>About</div></Link>
-                <Link to="/work" ><div>My Works</div></Link>
-                <Link to="/contact" ><div>Contact</div></Link>
-            </div>
-        </div>
-    )
+  return (
+    <div className="nav">
+      <div>
+        <ScrollLink to="home" smooth={true} offset={-50}>
+          <img className="logo" src={logo} alt="logo" />
+        </ScrollLink>
+      </div>
+      <div className="nav__side">
+        <ScrollLink to="home" smooth={true} offset={-50}>
+          <div className="nav__home">Home</div>
+        </ScrollLink>
+        {/* about id linked to scrolllink */}
+        <ScrollLink to="about" smooth={true} offset={-50}> 
+          <div className="nav__about">About</div>
+        </ScrollLink>
+        <div className="nav__contact"><a href="https://docs.google.com/document/d/1sLeu8KKqFF4cGS-zL7xrqLFb4uryli55/edit?usp=sharing&ouid=110595949783449698615&rtpof=true&sd=true" target="_blank" rel="noopener noreferrer">Resume</a></div>
+        
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
